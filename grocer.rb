d2a35d927b8ccbc924c3f6087f3221d81c579558
Ethan Fournier <ethanjosephfournier@gmail.com>
2020-03-05 21:57:36 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,6 +2,13 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  i = 0
+  while i < collection.length do
+    if collection[i][:item] === name
+      return collection[i]
+    end
+    i += 1
+  end
 end
 
 def consolidate_cart(cart)
@@ -9,12 +16,52 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  i = 0
+  
+  result = []
+  
+  while i < cart.length do
+    find_item_by_name_in_collection(cart[i][:item], result) ? 
+    find_item_by_name_in_collection(cart[i][:item], result)[:count] += 1 :
+    result << {
+      :item => cart[i][:item],
+      :price => cart[i][:price],
+      :clearance => cart[i][:clearance],
+      :count => 1
+      }
+     i += 1
+    end
+    
+    result
+
 end
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  i = 0
+  
+  while i < coupons.length do
+    this_item_hash = find_item_by_name_in_collection(coupons[i][:item],cart)
+    
+    this_item_hash[:count] > coupons[i][:num] ?
+    this_item_hash[:count] -= coupons[i][:num] :
+    cart.delete_at(cart.index(this_item_hash)) 
+    
+    cart << {
+      :item => "#{this_item_hash[:item]} W/COUPON",
+      :price => coupons[i][:cost] / coupons[i][:num],
+      :clearance => this_item_hash[:clearance],
+      :count => coupons[i][:num]
+    }
+    
+    i += 1
+    
+  end
+  
+  cart
+    
 end
 
 def apply_clearance(cart)
